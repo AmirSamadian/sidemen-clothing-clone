0e947f49082e8f8badd3b1fd79639e63c10093af
Amir Samadian <amir.samadian17@gmail.com>
2024-12-02 02:24:03 -0500

    A lot got done. Navbar almost complete I just need to make it responsive

---
+++ webapp/frontend/index.js
@@ -0,0 +1,9 @@
+
+function showSidebar() {
+    const sidebar = document.querySelector('.phone_menu');
+    sidebar.style.display = 'flex';
+}
+function closeSidebar() {
+    const sidebar = document.querySelector('.phone_menu');
+    sidebar.style.display = 'none';
+}
